1e4ddfc1e2c705fea65689b71f8458c7cc0587ad
Eric <ericblarson125@gmail.com>
2019-07-06 21:41:42 -0700

    Created an class abstractRaceScrape to house the recurring functions for yearly and weekly loads, and reloadDB

---
+++ abstractRaceScrape.py
@@ -0,0 +1,44 @@
+import requests
+import urllib.parse
+import os
+import re
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+
+
+class abstractRaceScrape:
+    def __init__(self):
+        self.archive_url = 'https://archives.amasupercross.com/'
+
+    def get_soup(self, link, selenium=False):
+        url = urllib.parse.urljoin(self.archive_url, link)
+
+        if selenium:
+            options = Options()
+            options.headless = True
+
+            path = os.path.join(os.getcwd(), 'geckodriver')
+            driver = webdriver.Firefox(options=options, executable_path=path)
+            driver.get(url)
+            return BeautifulSoup(driver.page_source, 'html.parser')
+
+        response = requests.get(url)
+        html = response.text
+        return BeautifulSoup(html, 'html.parser')
+
+    def get_races(self, year):
+        soup = self.get_soup('')
+        races = []
+        weeks = soup.find_all('table')[1].find_all("tr")[1:]
+
+        for week in weeks:
+            ################
+            # TODO
+            ###############
+            # Remove the Monster Energy Cup races
+            yearRegex = re.compile('^' + year)
+            races.append(week.find('a', href=yearRegex).string)
+
+        return races[:len(races) - 1]
