bb52ed1346723639de2a74a32707cfdd098b2aec
ericbl125 <ericblarson125@gmail.com>
2019-07-17 09:33:11 -0700

    Merge branch 'master' of https://github.com/ericbl125/sxstats
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/sxstats.iml
    #	StatsDB.py

--- abstractRaceScrape.py
+++ abstractRaceScrape.py
@@ -3,6 +3,7 @@ import urllib.parse
 import os
 import re
 
+from sys import platform
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
@@ -12,6 +13,8 @@ class abstractRaceScrape:
     def __init__(self):
         self.archive_url = 'https://archives.amasupercross.com/'
 
+    #TODO add error handling when getting soup to retry in different ways and manners without crashing whole program
+    # TODO example would no connection
     def get_soup(self, link, selenium=False):
         url = urllib.parse.urljoin(self.archive_url, link)
 
@@ -20,6 +23,8 @@ class abstractRaceScrape:
             options.headless = True
 
             path = os.path.join(os.getcwd(), 'geckodriver')
+            gecko = self.get_geckodriver()
+            path = os.path.join(os.getcwd(), gecko)
             driver = webdriver.Firefox(options=options, executable_path=path)
             driver.get(url)
             return BeautifulSoup(driver.page_source, 'html.parser')
@@ -42,3 +47,12 @@ class abstractRaceScrape:
             races.append(week.find('a', href=yearRegex).string)
 
         return races[:len(races) - 1]
+
+    @staticmethod
+    def get_geckodriver():
+        if platform == "linux" or platform == "linux2":
+            return 'geckodriver_lin'
+        elif platform == "darwin":
+            return 'geckodriver_mac'
+        elif platform == "win32":
+            return 'geckodriver_win.exe'
